cef33a95a018b390ba1d109b97f3055d6bc7e3bf
ssabii <fhy1930@gmail.com>
2022-11-30 00:23:07 +0900

    feat: todo list 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import { v4 as uuid } from "uuid";
+import React, { useState } from "react";
 import Add from "./components/Add";
 import "./App.css";
 
-function App() {
+interface Todo {
+  key: string;
+  value: string;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (newTodo: string) => {
+    setTodos((prev) => [...prev, { key: uuid(), value: newTodo }]);
+  };
+
   return (
     <div className="App">
-      <Add />
+      <header className="App-header">
+        <h1 data-testid="header-title">Todo List</h1>
+      </header>
+      <main>
+        <ul data-testid="list">
+          {todos.map((todo) => (
+            <li key={todo.key}>{todo.value}</li>
+          ))}
+        </ul>
+        <Add onAdd={handleAdd} />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
